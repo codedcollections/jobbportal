573ecf998d79c9e9b1d3993763fe35a0389fea0a
codedcollections <ma.unander@gmail.com>
2026-09-03 16:06:42 +0200

    add basic home and components

---
+++ components/Hero.jsx
@@ -0,0 +1,29 @@
+import {
+  storyblokEditable,
+  StoryblokServerComponent,
+} from "@storyblok/react/rsc"
+
+export default function Hero({ blok }) {
+  const backgroundStyle = blok.backgroundImage?.filename
+    ? {
+        backgroundImage: `url(${blok.backgroundImage.filename})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        /* 				backgroundImage: `url(${blok.backgroundImage.filename})`,
+				
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',*/
+      }
+    : undefined
+
+  return (
+    <section {...storyblokEditable(blok)} style={backgroundStyle}>
+      <h1>{blok.title}</h1>
+      <p>{blok.description}</p>
+      {blok.blocks?.map((nestedBlok) => (
+        <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+      ))}
+    </section>
+  )
+}
---
+++ components/Page.jsx
@@ -0,0 +1,18 @@
+import {
+  storyblokEditable,
+  StoryblokServerComponent,
+} from "@storyblok/react/rsc"
+
+export default function Page({ blok, ...rest }) {
+  return (
+    <main {...storyblokEditable(blok)}>
+      {blok.body?.map((nestedBlok) => (
+        <StoryblokServerComponent
+          blok={nestedBlok}
+          key={nestedBlok._uid}
+          {...rest}
+        />
+      ))}
+    </main>
+  )
+}
